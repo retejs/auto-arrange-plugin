f3dc7d24e6bcc3f75cd24fa65bf6f8a1cb6a7d61
Vitaliy Stoliarov <huccah@gmail.com>
2019-04-23 21:52:09 +0300

    v0.3.0, arrange event

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { AutoArrange } from './auto-arrange';
 
-
 function install(editor, { margin = { x: 50, y: 50 }, depth = null }) {
+    editor.bind('arrange');
 
     const ar = new AutoArrange(editor, margin, depth);
+    
+    editor.on('arrange', ({ node }) => ar.arrange(node));
 
-    editor.arrange = ar.arrange.bind(ar);
+    editor.arrange = node => {
+        console.log(`Deprecated: use editor.trigger('arrange', { node }) instead`);
+        ar.arrange(node);
+    }
 }
 
 export default {
