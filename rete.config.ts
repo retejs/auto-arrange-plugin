ccf75a4971cb5d6a051bf3035c8dbdf179c9909e
Ni55aN <huccah@gmail.com>
2023-04-10 14:49:19 +0300

    v2.0.0-beta.6

--- rete.config.ts
+++ rete.config.ts
@@ -3,7 +3,7 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
   input: 'src/index.ts',
-  name: 'AutoArrangePlugin',
+  name: 'ReteAutoArrangePlugin',
   globals: {
     'elkjs': 'ELK',
     'rete': 'Rete',
